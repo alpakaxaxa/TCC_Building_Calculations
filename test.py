92688775d70cadcaf743aef47d1a189e8b2f9adb
alpakaxaxa <34175210+alpakaxaxa@users.noreply.github.com>
2020-04-05 01:31:50 +0200

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-from classes import BuildingInformationData, BuildingInformationData
-import json
-
-d = BuildingInformationData("./data.json")
-d.addRecord(77, 'appartment')
-print(d.buildingInformation())
